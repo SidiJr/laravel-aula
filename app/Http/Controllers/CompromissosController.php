0c56bca1c63aac4f692a550c781870a3202bb2e7
Sidi <sednei.jr@gmail.com>
2025-05-07 19:49:30 -0300

    feat: :tada: Iniciando
    Iniciando projeto.

---
+++ app/Http/Controllers/CompromissosController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CompromissosController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function salvar()
+    {
+    }
+
+    public function editar()
+    {
+    }
+
+    public function apagar()
+    {
+    }
+}
